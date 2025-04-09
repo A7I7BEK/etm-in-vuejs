52c86b7081ba23b4a7da2ebf684787fada831465
Azizbek Iskandar <a7i7bek@gmail.com>
2025-04-09 22:07:40 +0500

    refactor(SocketService): improve code readability and structure

--- src/services/SocketService.js
+++ src/services/SocketService.js
@@ -1,20 +1,30 @@
 import { io } from 'socket.io-client';
 
 
-export class SocketService
-{
-	constructor ({ url, path, token, roomId })
-	{
+export class SocketService {
+	socket;
+	url;
+	path;
+	token;
+	roomId;
+
+	constructor({ url, path, token, roomId }) {
 		this.url = url;
 		this.path = path;
+		this.token = token;
+		this.roomId = roomId;
+		this.setup();
+	}
 
-		this.socket = io(url, {
-			path,
+
+	setup() {
+		this.socket = io(this.url, {
+			path: this.path,
 			query: {
-				roomId,
+				roomId: this.roomId,
 			},
 			auth: {
-				token,
+				token: this.token,
 			},
 			forceNew: true,
 			autoConnect: false,
@@ -22,31 +32,26 @@ export class SocketService
 	}
 
 
-	connect()
-	{
+	connect() {
 		this.socket.connect();
 	}
 
 
-	disconnect()
-	{
+	disconnect() {
 		this.socket.disconnect();
 		this.socket.off();
 	}
 
 
-	enableMonitoring()
-	{
-		this.socket.on('connect', () =>
-		{
+	enableMonitoring() {
+		this.socket.on('connect', () => {
 			console.log(`{${this.url + this.path}} [connect] Socket Id: ${this.socket.id}`);
 
 			const engine = this.socket.io.engine;
 			console.log(`{${this.url + this.path}} [connect] Engine Transport: ${engine.transport.name}`);
 			// in most cases, prints "polling"
 
-			engine.once('upgrade', () =>
-			{
+			engine.once('upgrade', () => {
 				/**
 				 * called when the transport is upgraded (i.e. from HTTP long-polling to WebSocket)
 				 */
@@ -56,18 +61,15 @@ export class SocketService
 			});
 		});
 
-		this.socket.on('connect_error', (error) =>
-		{
-			if (this.socket.active)
-			{
+		this.socket.on('connect_error', (error) => {
+			if (this.socket.active) {
 				/**
 				 * temporary failure, the socket will automatically try to reconnect
 				 */
 
 				console.log(`{${this.url + this.path}} [connect_error] temporary...`);
 			}
-			else
-			{
+			else {
 				/**
 				 * the connection was denied by the server
 				 * in that case, `socket.connect()` must be manually called in order to reconnect
@@ -77,18 +79,15 @@ export class SocketService
 			}
 		});
 
-		this.socket.on('disconnect', (reason) =>
-		{
-			if (this.socket.active)
-			{
+		this.socket.on('disconnect', (reason) => {
+			if (this.socket.active) {
 				/**
 				 * temporary disconnection, the socket will automatically try to reconnect
 				 */
 
 				console.log(`{${this.url + this.path}} [disconnect] temporary...`);
 			}
-			else
-			{
+			else {
 				/**
 				 * the connection was forcefully closed by the server or the client itself
 				 * in that case, `socket.connect()` must be manually called in order to reconnect
